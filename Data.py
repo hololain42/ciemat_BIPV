d3798fd1e793a8e6f96d4eab564679af7d8ebb21
hololain42 <manrod22@ucm.es>
2025-06-06 18:55:46 +0200

    Mejora del procesamiento del datalogger, añadidos logs detallando el proceso y manejo de errores en archivos

--- Data.py
+++ Data.py
@@ -5,6 +5,7 @@ import matplotlib.dates as mdates
 import pandas as pd
 import os 
 import chardet
+import sys
 #%%
 
 ##### IMPORT DATA SECTION #####
@@ -35,13 +36,13 @@ Canal 305-Canal_305_G2_arriba_[W/m2]                    Canal 304-Canal_304_G1_a
 Esto ocurre principalmente con archivos antiguos ya que a partir de enero del presente año
 las cabeceras corresponden en longitud al número de registros de datos pero esto sigue siendo 
 un error ya que añade el canal 420 (suma de canales 101+102) los cuales habían sido liberados ya
-hace tiempoo, entonces se corregirán las cabeceras para ambos casos.
+hace tiempo, entonces se corregirán las cabeceras para ambos casos.
 
 Si bien José corrigió la no existencia del canal 220, no estoy seguro si al añadir un canal ocasione
 un desplazamiento en las cabeceras, es decir, aumentando en 1 el número de cabeceras por encima del número
 de registros (debido a la presencia del canal 420 el cual no es relevante en lo absoluto). 
 
-La corrección realizanda en el programa solventa ambas situaciones y dejará evaluar sin problemas los
+La corrección realizada en el programa solventa ambas situaciones y dejará evaluar sin problemas los
 registros que no tengan problemas en adelante.
 '''
 
@@ -62,38 +63,105 @@ registros que no tengan problemas en adelante.
 #        df = df.drop(columns=[None])
 #    DataLoggerDataFrame = pd.concat([DataLoggerDataFrame, df], axis=0, ignore_index=True)
 
-path = os.getcwd() 
-print(path)
+path = os.getcwd()
 Dataloggerfiles = os.listdir(path+"/Datos Datalogger/DAQ970A")
 
 DataLoggerDataFrame = pd.DataFrame()
 
+num_arch = len(Dataloggerfiles)
+print("-" * 50)
+print(f"Total de archivos del Datalogger a analizar: {num_arch}")
+print(f"->Datos del " + Dataloggerfiles[1].split("_DAQ970A_Colores")[0] + " al " + Dataloggerfiles[-1].split("_DAQ970A_Colores")[0])
+print("-" * 50)
+
+# Loggear la primera aparición del canal 220 (Potencia AC Inversor Huawei)
+primer_canal_220 = None
+# Lista para posibles archivos que fallen al procesarse
+fallo_lectura_arch = []
+# Contador de iteraciones
+iter = 0
+
+print(f"[INFO] Comienza el procesado de archivos:")
 for i in Dataloggerfiles[30:]:
-    with open("Datos Datalogger/DAQ970A/" + i, 'r', encoding='latin1') as f:
-        columns = f.readlines()
-    lt = [o.strip().split('\t') for o in columns]
-    header = lt[0]
 
-    if 'Canal 220-Canal_220_Potencia AC_InversorHuawei_[W] ' in header:
-        pass
-    else:
-        idx = header.index("Canal 219-Canal_219_Ientradainversor_terracota_[A] ")
-        header.insert(idx+1, "Canal 220-Canal_220_Potencia AC_InversorHuawei_[W] ")
-        if 'Canal 420-Canal_420_101+102' in header:
-            header = header[:-1]
-    
-    new_lt = []; new_lt.append(header); new_lt = new_lt + lt[1:]
+    try:
+        # print(f"Abriendo archivo {i}")
 
-    df = pd.DataFrame(new_lt)
-    df = df[1:]
-    df.columns = header
+        with open("Datos Datalogger/DAQ970A/" + i, 'r', encoding='latin1') as f:
+            columns = f.readlines()
+        lt = [o.strip().split('\t') for o in columns]
+        header = lt[0]
 
-    if None in df.columns:
-        df = df.drop(columns=[None])
+        # Comprobamos si el Canal 220 existe en archivo. Si no, lo insertamos después del canal 219.
+        if 'Canal 220-Canal_220_Potencia AC_InversorHuawei_[W] ' in header:
+            if primer_canal_220 is None:
+                # print(f"[INFO] Canal 220-Canal_220_Potencia AC_InversorHuawei_[W] ya existe en el archivo {i}")
+                primer_canal_220 = i
+
+            pass
+
+        else:
+            # Insertamos el canal 220 en el header
+            idx = header.index("Canal 219-Canal_219_Ientradainversor_terracota_[A] ")
+            header.insert(idx+1, "Canal 220-Canal_220_Potencia AC_InversorHuawei_[W] ")
+            
+            if 'Canal 420-Canal_420_101+102' in header:
+                # Eliminamos el canal 420 del header (suma de canales 101+102, los cuales ya no se usan)
+                header = header[:-1]
         
-    DataLoggerDataFrame = pd.concat([DataLoggerDataFrame, df], axis=0, ignore_index=True)
+        # Creamos nueva lista con el header corregido (adaptado para todos)
+        new_lt = []; new_lt.append(header); new_lt = new_lt + lt[1:]
+
+        # Creamos el dataframe nuevo
+        df = pd.DataFrame(new_lt)
+        df = df[1:] # Elimina la primera fila (para no duplicar el encabezado)
+        df.columns = header # Asigna nombres a las columnas
+
+        # Eliminamos posibles columnas vacías
+        if None in df.columns:
+            df = df.drop(columns=[None])
+            print(" [HECHO] Columnas vacías del dataframe eliminadas")
+            
+        DataLoggerDataFrame = pd.concat([DataLoggerDataFrame, df], axis=0, ignore_index=True)
+
+        iter += 1
+        # Mostrar progreso cada X archivos
+        if (iter) % 30 == 0:  # Cada 30 archivos
+            print(f"Procesados {iter+1}/{num_arch} archivos.")
+    
+    except Exception as e:
+        print(f"ERROR procesando el archivo {i} - {type(e).__name__}")
+        #print(f"     Mensaje: {str(e)}")
+        # Agregamos el archivo fallido a la lista:
+        fallo_lectura_arch.append({
+            'nombre': i,
+            'tipo_error': type(e).__name__,
+            'mensaje_error': str(e)
+            })
+        continue
+
+print("-" * 50)
+print(f"Procesamiento del header completado! {iter} archivos en total.")
+print(f"Dataframe creado: {DataLoggerDataFrame.shape[0]} filas x {DataLoggerDataFrame.shape[1]} columnas")
+print("-" * 50)
+
+# Posible fallo de lectura en archivos
+if fallo_lectura_arch:
+    print(f"[ERROR] FALLO DE LECTURA EN {len(fallo_lectura_arch)} ARCHIVO(S):")
+    for archivo in fallo_lectura_arch:
+        print(f"   - {archivo['nombre']}")
+        print(f"     Tipo de error: {archivo['tipo_error']}")
+        print(f"     Mensaje de error: {archivo['mensaje_error']}")
+else:
+    print(f"\n Todos los archivos se procesaron correctamente")
+print("-" * 50)
 
 
- 
+# Información canal 220
+if primer_canal_220 is not None:
+    print(f"[INFO] Primera aparición del canal 220 (Potencia AC Inversor Huawei): " + primer_canal_220)
+    print(f"   A partir de ese archivo ya se loggea la información del inversor.\n")
+print("-" * 50)
+
 
 # %%
